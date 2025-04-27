750f309e85b23600ea27cb5515f08c1ee3d3efdd
Reykel Raflen Awang <reykel210618@gmail.com>
2025-04-27 18:27:39 +0700

    commit

--- Case_B/resources/views/layouts/app.blade.php
+++ Case_B/resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        /* semua CSS kamu tetap ada, tidak ada yang dihapus */
+        
         html, body {
             height: 100%;
             margin: 0;
